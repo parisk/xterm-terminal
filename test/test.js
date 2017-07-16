d4a40a4e5b5d2284b8b989cef6306276260f12a2
Paris Kasidiaris <paris@sourcelair.com>
2017-07-16 16:13:08 +0300

    Add Travis CI tests

---
+++ test/test.js
@@ -0,0 +1,7 @@
+describe('xterm-terminal', () => {
+  describe('#constructor()', () => {
+    it('should set `._xterm` to a Terminal instance', () => {
+      // TODO: Write this
+    });
+  });
+});
