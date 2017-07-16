0d306907d1f2de812504af43fbaa360e070d720a
Paris Kasidiaris <paris@sourcelair.com>
2017-07-16 17:45:51 +0300

    Add xterm.js to included files

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'chai'],
-    files: ['test/**/*.js', 'xterm-terminal.js'],
+    files: ['test/**/*.js', 'node_modules/xterm/dist/xterm.js', 'xterm-terminal.js'],
     reporters: ['progress'],
     port: 9876,  // karma web server port
     colors: true,
