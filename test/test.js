9257e9fa53b597ae4538e1255f8652e5a2f27b97
Paris Kasidiaris <paris@sourcelair.com>
2017-07-17 23:39:44 +0300

    Start implementing public API tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,32 @@
 describe('xterm-terminal', () => {
-  describe('#constructor()', () => {
-    it('should set `._xterm` to a Terminal instance', () => {
-      let term = document.createElement('xterm-terminal');
+  let term;
 
+  describe('#constructor()', () => {
+    beforeEach(() => {
+      term = document.createElement('xterm-terminal');
+    });
+
+    it('should set `._xterm` to a Terminal instance', () => {
       document.body.appendChild(term);
       assert.exists(term._xterm);
+    });
+  });
+
+  describe('xterm.js public API implementation', () => {
+    let proxySpy;
+
+    beforeEach(() => {
+      term = document.createElement('xterm-terminal');
+      proxySpy = chai.spy(term._proxy);
+      term._proxy = proxySpy;
+    });
+
+    it('#attachCustomKeydownHandler', () => {
+      let customKeyDownHandler = () => {};
+
+      term.attachCustomKeydownHandler(customKeyDownHandler);
+
+      expect(proxySpy).to.have.been.called.with([customKeyDownHandler]);
     });
   });
 });
